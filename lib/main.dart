4280bd9adc458d1adfeb4a736e7cb6e87ffcc793
Sidharth K V <sidharthkv1100@gmail.com>
2026-01-02 10:30:17 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
   runApp(const MyApp());
 }
 
@@ -15,16 +17,4 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            "🔥 Firebase Initialized Successfully",
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-      ),
-    );
-  }
-}
+    return MaterialApp
